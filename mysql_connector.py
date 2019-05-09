fe1e102b433842e74d8f4860b4ad85ce6616e014
ericgaspar <46165813+ericgaspar@users.noreply.github.com>
2019-05-09 20:37:06 +0200

    first commit

---
+++ mysql_connector.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import glob, os, time, mysql.connector
+
+
+
+rfiddb = mysql.connector.connect(host    = 'localhost',   	# Mettre l'adresse du server mysql (192.168.0.38)
+		      		user     = 'root', 		# Mettre le nom du client (pi)
+			    	password = 'pythagore',		# Mettre le mot de passe (raspberry)
+	    			database = 'rfid_data')	# Mettre le nom de la base de donnee (meteo_data)
