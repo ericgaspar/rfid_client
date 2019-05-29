69d06e2ca2e3ae04c37849b7304d93680a4ffea9
ericgaspar <46165813+ericgaspar@users.noreply.github.com>
2019-05-29 16:16:55 +0200

    Update mysql_connector.py

--- mysql_connector.py
+++ mysql_connector.py
@@ -3,9 +3,7 @@
 
 import glob, os, time, mysql.connector
 
-
-
-rfiddb = mysql.connector.connect(host    = 'localhost',   	# Mettre l'adresse du server mysql (192.168.0.38)
-		      		user     = 'root', 		# Mettre le nom du client (pi)
-			    	password = 'password',		# Mettre le mot de passe (raspberry)
-	    			database = 'rfid_data')	# Mettre le nom de la base de donnee (meteo_data)
+rfiddb = mysql.connector.connect(host    = 'localhost',   	# Mettre l'adresse du server mysql
+		      		user     = 'root', 		# Mettre le nom du client
+			    	password = 'password',		# Mettre le mot de passe
+	    			database = 'RFID_DB')		# Mettre le nom de la base de donnee
